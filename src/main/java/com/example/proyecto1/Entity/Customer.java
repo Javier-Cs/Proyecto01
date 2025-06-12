2a22902726a98ead32da0746ab8726986b3f078a
Javier-Cs <krchipullajavier@gmail.com>
2025-06-11 19:17:51 -0500

    nuevo proyecto con spring notaciones para la implementacion con la base de datos. mapeo de entidad

---
+++ src/main/java/com/example/proyecto1/Entity/Customer.java
@@ -0,0 +1,24 @@
+package com.example.proyecto1.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "customer_tbl")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customerId")
+    private long idCostumer;
+    @Column(name = "firstName")
+    private String firstName;
+    @Column(name = "lastName")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+}
